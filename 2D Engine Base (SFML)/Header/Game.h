283c9a9bb6b68c7a21bfaa0b450f86ef552dbc10
Kevin Hall <omegak@live.com>
2020-11-25 15:53:11 -0500

    Added some comments

--- 2D Engine Base (SFML)/Header/Game.h
+++ 2D Engine Base (SFML)/Header/Game.h
@@ -4,9 +4,9 @@
 class Game
 {
 private:
-	sf::RenderWindow* window_handle;
-	sf::Clock clock;
-	sf::Vector2u screen_size;
+	sf::RenderWindow* window_handle;	// Handle to SFML window
+	sf::Clock clock;					// SFML Clock
+	sf::Vector2u screen_size;			// Screen Size (Reserved for future use)
 public:
 	Game(sf::RenderWindow *window_handle, sf::Vector2u screen_size);
 	sf::Clock& getClock();
--- 2D Engine Base (SFML)/Header/Tile.h
+++ 2D Engine Base (SFML)/Header/Tile.h
@@ -6,15 +6,15 @@
 class Tile
 {
 private:
-	TileReference* container;
-	sf::Vertex* quad;
+	TileReference* container;	// Container Tile
+	sf::Vertex* quad;			// Pointer to Quad
 	//TODO: Object Attachment
+	void updateFrame();			// Update the frame if needed
 public:
 	Tile(TileSet &ts, int tile_index, sf::Vertex *quad);
 	Tile(TileReference& tile, sf::Vertex* quad);
 	TileReference& get();
 	void setVertexPointer(sf::Vertex* quad);
 	sf::Vertex*& getVertexPointer();
-	void updateFrame();
 	std::string to_string() const;
 };
